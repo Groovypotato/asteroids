abfceafa6e1340ef227da09a0cc633ca3414afd9
Groovypotato <gleasoncr@gmail.com>
2025-03-10 04:21:09 -0500

    circleshape,player,updated constants

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    dt = 0 
+    g_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         g_color = (12,12,12)
         screen.fill(g_color)
+        player.draw(screen)
         pygame.display.flip() 
+        dt = (g_clock.tick(60) / 1000)
+        
 
 if __name__ == "__main__":
 
